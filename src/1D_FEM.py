64102f50499342d97ffa50de5d4d0f098b791c2d
Johann Maree <jnmaree@protonmail.com>
2021-07-04 22:58:39 +0200

    udpate to FEM mesh methods

--- src/1D_FEM.py
+++ src/1D_FEM.py
@@ -1,21 +1,39 @@
 import numpy
 
-def generate_nodes(dimension_x, num_of_elements):
-    node_coordinates = numpy.array((num_of_elements + 1))
-    dim_increment = dimension_x/num_of_elements
-    node_coordinates[0] = 0;
-    for i in range (1, num_of_elements + 1):
-        node_coordinates[i] = dim_increment
-        dim_increment += dim_increment
-    return node_coordinates
+class Mesh_1D:
 
-def generate_elements(node_array):
-    node_end = node_array.size - 1
-    element_array = numpy.array((node_end, 2))
-    for i in range(0, node_end):
-        element_array[i, 0] = node_array[i]
-        element_array[i, 1] = node_array[i + 1]
-    return element_array
+    dimension_x = 0
+    n_elements = 0
+    n_nodes = 0
+    mesh_order = 1
+    
+    nodes_array = numpy.array([])
+    elements_array = numpy.array([])
+    
+    def __init__(self, x_dimension, num_of_elements, mesh_order = 1):
+        self.dimension_x = x_dimension
+        self.n_elements = num_of_elements
+        if mesh_order == 1:
+            self.n_nodes = num_of_elements + 1
+        self.mesh_order = mesh_order
+        self.generate_nodes()
+        self.generate_elements()
+    
+    def generate_nodes(self):
+        self.nodes_array = numpy.array((self.n_nodes))
+        dim_increment = self.dimension_x/self.n_elements
+        self.nodes_array[0] = 0
+        for i in range (1, self.n_nodes):
+            self.nodes_array[i] = dim_increment
+            dim_increment += dim_increment
+
+    def generate_elements(self):
+        node_end = self.n_nodes - 1
+        element_array = numpy.array((node_end, 2))
+        for i in range(0, node_end):
+            element_array[i, 0] = self.nodes_array[i]
+            element_array[i, 1] = self.nodes_array[i + 1]
+        return element_array
 
 def generate_basis_functions(element_array, function_order = 1):
     if function_order == 1:
@@ -45,7 +63,7 @@ def main():
     
     # 'Mesh' parameters
     X_dimension = 10    # distance in meters
-    N_nodes = 8         # number of nodes in domain
+    N_elements = 8      # number of elements in domain
     
     # Analysis conditions
     BC_type1 = 40       # Type 1 (Dirichlet) boundary condition
@@ -57,8 +75,7 @@ def main():
     K_ = 20             # Stiffness Coefficient (Material Property)
     
     # Code execution:
-    nodes = generate_nodes(X_dimension, N_nodes)
-    elements = generate_elements(nodes)
+    mesh_test = Mesh_1D(X_dimension, N_elements)
     
     
     
