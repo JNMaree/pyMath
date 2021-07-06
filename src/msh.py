3786b8b308e984bf1fb3d78b091055663a8715af
Johann Maree <jnmaree@protonmail.com>
2021-07-06 23:35:30 +0200

    mesh format creation

--- src/msh.py
+++ src/msh.py
@@ -1,6 +1,16 @@
 
-def read_msh4(filename):
-    pass
+class mesh:
+
+    n_nodes = 0
+    node_array = numpy.zeros([0, 3])
+
+    n_elements = 0
+    element_array = 
+    
+    def __init__(self, filename):
+
+class element:
+    element_type = 0
 
 def main():
 	pass
