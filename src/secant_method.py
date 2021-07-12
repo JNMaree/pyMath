98a37e99d3ddcb060a35b658b5401f53a27311cd
Johann Maree <jnmaree@protonmail.com>
2021-07-12 22:31:15 +0200

    update to secant method and added unit test

--- src/secant_method.py
+++ src/secant_method.py
@@ -1,19 +1,21 @@
 from polynomial import Polynomial
 import numpy
 
-error_tolerance = 1e-3
+error_tolerance = 1e-6
 max_iterations = 30
 
-def secant_method(polynomi, x0_estimate, x1_estimate):
+def secant_method(polynomi, x0_estimate, x1_estimate, iterations = max_iterations):
     i = 0
     diff = 1
     x0 = x0_estimate
     x1 = x1_estimate
-    while i < max_iterations and diff > error_tolerance:
-        x2 = x1 - polynomi.calculate(x1)*(x1 - x0)/(polynomi.calculate(x1) - polynomi.calculate(x0))
-        diff = x2 - x1
+    while i < iterations and diff > error_tolerance:
+        x2 = x1 - polynomi.calculate(x1) * (x1 - x0) / (polynomi.calculate(x1) - polynomi.calculate(x0))
         x0 = x1
         x1 = x2
+        diff = abs(x1 - x0)
+        # print("iteration:", i, ", diff:", diff)
+        i += 1
     return x1
     
 def is_approximately_equal(a, b):
@@ -21,3 +23,12 @@ def is_approximately_equal(a, b):
         return True
     else:
         return False
+
+def main():
+    #test
+    polynom = Polynomial([-612, 0, 1])
+    root = secant_method(polynom, 10, 30, 5)
+    print("ROot:", root)
+
+if __name__ == "__main__":
+    main()
