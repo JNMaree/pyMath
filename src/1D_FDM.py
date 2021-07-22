09ba98c70e2afc626214e49f58722590cf925b2c
Johann Maree <jnmaree@protonmail.com>
2021-07-22 23:45:20 +0200

    change to FEM and FDM methods to utilise update to Mesh1D functions

--- src/1D_FDM.py
+++ src/1D_FDM.py
@@ -1,5 +1,5 @@
 import numpy
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as pyplot
 
 from mesh_1D import Mesh1D, NodeSpace1D, ElementSpace1D
 
@@ -7,21 +7,27 @@ class FiniteDifferenceMethod(Mesh1D):
 
     solution_space = []
 
-    material_properties = 0
-    
-    def __init__(self, mesh, material_properties, type1bcs, type2bcs):
-        self.mesh = mesh
-        self.solution_space = NodeSpace1D(mesh)
-        self.material_properties = material_properties
-        self.type1BC = type1bcs
-        self.type2BC = type2bcs
+    material_property = 0
 
+    type1BC = []
+    type2BC = []
+    
+    def __init__(self, mesh, material_properties, bc_type1, bc_type2):
+        self.mesh1D = mesh
+        self.solution_space = NodeSpace1D(mesh)
+        self.material_property = material_properties
+        self.type1BC = bc_type1
+        self.type2BC = bc_type2
+
+    # Solve the differential equations using a Forward-Time, Centred-Space (FTCS) scheme
     def solve(self):
-        for i in range(self.solution_space):
-            pass
+        temp_solution_space = self.solution_space
+        for i in range(self.n_nodes):
+            self.solution_space[i] = temp_solution_space[i] + self.material_property
+            temp_solution_space[i] = self.solution_space[i]
         
     def plot(self):
-        plot.plot(self.node_array, self.solution_space)
+        pyplot.plot(self.nodes, self.solution_space)
 
 
 def main():
@@ -30,8 +36,8 @@ def main():
     X_dimension = 12    # Distance specification (meters)
     N_elements = 20     # Number of finite elements in mesh
 
-    # Create mesh using provided parameters
-    finiteMesh = Mesh1D(X_dimension, N_elements)
+    # Create mesh of discrete elements that consist of two nodes per element
+    fdm_mesh = Mesh1D(X_dimension, N_elements)
 
     # - Analysis Conditions:
     #   $ Material properties:
@@ -40,16 +46,16 @@ def main():
     #   $ Type 1 (Dirichlet) Boundary Conditions(BCs):
     Type1_BC = 24       # Temperature specification
     Type1_Nodes = [0]   # Node indices subject to Type 1 BC
-    BC_Type1 = NodeSpace1D(finiteMesh.n_nodes)
+    BC_Type1 = NodeSpace1D(fdm_mesh.n_nodes)
     BC_Type1.assign_values(Type1_BC, Type1_Nodes)
     
     #   $ Type 2 (Neumann) Boundary Conditions(BCs):
     Type2_BC = 16                   # Heat Flux specification
     Type2_Nodes = [N_elements - 1]  # Node indices subject to Type 2 BC
-    BC_Type2 = NodeSpace1D(finiteMesh.n_nodes)
+    BC_Type2 = NodeSpace1D(fdm_mesh.n_nodes)
     BC_Type2.assign_values(Type2_BC, Type2_Nodes)
     
-    FDM = FiniteDifferenceMethod(finiteMesh, K, BC_Type1, BC_Type2)
+    FDM = FiniteDifferenceMethod(fdm_mesh, K, BC_Type1, BC_Type2)
     FDM.solve()
     FDM.plot()
 
--- src/mesh_1D.py
+++ src/mesh_1D.py
@@ -24,6 +24,11 @@ class NodeSpace1D:
         for i in node_indices:
             self.nodes[i] = num_value
 
+    def __getitem__(self, key):
+        return self.nodes[key]
+    def __setitem__(self, key, value):
+        self.nodes[key] = value
+        
 class ElementSpace1D:
 
     # Element array:
@@ -67,8 +72,7 @@ class Mesh1D(NodeSpace1D, ElementSpace1D):
             self.n_nodes = num_of_elements + 1
         self.mesh_order = mesh_order
 
-    # generate nodes (2 per element) to be equal distances apart, spanning total length of x
-    def generate_equidistant_nodes(self):
+        # generate nodes to be equal distances apart, spanning total length of x
         self.node_array = numpy.array((self.n_nodes))
         dim_increment = self.dimension_x/self.n_elements
         self.node_array[0] = 0
@@ -76,7 +80,7 @@ class Mesh1D(NodeSpace1D, ElementSpace1D):
             self.node_array[i] = dim_increment
             dim_increment += dim_increment
 
-    # generate elements to 
+    # generate elements to contain a specified number of nodes
     def generate_elements(self, nodes_per_element = 2):
         node_end = self.n_nodes - 1
         self.element_array = numpy.array((node_end, nodes_per_element))
