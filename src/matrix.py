f60befaee8e79cc414f1128ec5ae9dcf88ce0586
Johann Maree <jnmaree@protonmail.com>
2021-08-06 20:19:26 +0200

    update to matrix methods: added matrix as class with matrix manip methods

--- src/matrix.py
+++ src/matrix.py
@@ -1,38 +1,68 @@
 import numpy
 
-def to_reduced_row_echelon_form(matrix):
-    lead = 0
-    for r in range(matrix.shape[0]):
-        if matrix.shape[1] <= lead:
-            break
-        while matrix[r, lead]:
-            i = r + 1
-            if matrix.shape[0] == i:
-                i = r
-                lead += 1
-                if matrix.shape[1] == lead:
-                    break
+class Matrix:
 
-def inverse_of(matrix):
-    # Check if 2D and nxn
-    if matrix.ndim == 2 and matrix.shape[0] == matrix.shape[1]:
-        # Using a numpy function
-        ret_matrix = numpy.linalg.inv(matrix)
+    matrix = []
+
+    def __init__(self, array):
+        if isinstance(array, numpy.ndarray):
+            self.matrix = array
+        elif isinstance(array, list):
+            self.matrix = numpy.array(array)
+
+    # Overload [] operator
+    def __getitem__(self, key):
+        return self.matrix[key]
+    def __setitem__(self, key, value):
+        self.matrix[key] = value
+
+    # Reduce a matrix to its reduced row echelon form
+    def reduced_row_echelon(self):
+        lead = 0
+        stopCondition = False
+        ret_matrix = self.matrix
+
+        # matrix loop
+        for r in range(self.matrix.shape[0]):
+            if self.matrix.shape[1] <= lead:
+                break
+            while self.matrix[r, lead] == 0 and not stopCondition:
+                i = r + 1
+                if self.matrix.shape[0] == i:
+                    i = r
+                    lead += 1
+                    if self.matrix.shape[1] == lead:
+                        stopCondition = True
+            if i != r:
+                self.matrix[[i, r],:] = self.matrix[[r, i], :]
+            self.matrix[r, :] /= self.matrix[r, lead]
+
         return ret_matrix
-    else:
-        raise ArithmeticError    
+            
+        
+    def inverse(self):
+        # Check if 2D and nxn (square)
+        if self.matrix.ndim == 2 and self.matrix.shape[0] == self.matrix.shape[1]:
+            # Using a numpy function
+            ret_matrix = numpy.linalg.inv(self.matrix)
+            return ret_matrix
+        else:
+            raise ArithmeticError    
                 
 
 def main():
     test_matrix = numpy.array(([1, 1, 3], [0, 2, 4], [-1, 1, 0]))
     print("Test_Matrix:")
-    print(test_matrix)
-    inverse_matrix = inverse_of(test_matrix)
+    m = Matrix(test_matrix)
+
+    # Test Reduced Row Echelon method
+    print("RREchelon Form:")
+    print(m.reduced_row_echelon)
+
+    # Test Inverse method
     print("Inv_Matrix:")
-    print(inverse_matrix)
-    #inv_test_matrix = inverse_of(test_matrix)
-    #print("Inverse TestMatrix:")
-    #print(inv_test_matrix)
+    print(m.inverse)
+    
 
 if __name__ == "__main__":
     main()
