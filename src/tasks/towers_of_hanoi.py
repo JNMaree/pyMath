5c0e3d5cd835238cd678658403a39da935fbc711
Johann Maree <jnmaree@protonmail.com>
2021-10-21 11:51:13 +0200

    Tower Of Hanoi solution for n=3

--- src/tasks/towers_of_hanoi.py
+++ src/tasks/towers_of_hanoi.py
@@ -4,20 +4,27 @@ class TowersOfHanoi:
     # Define the number of disk levels
     n = 0
 
+    # Set Default value as bigger than biggest n value
+    #   - for comparison purposes; ie:
+    #       if disk < zero then ovailable to move there
+    zero = n + 1
+
     # Define the 2D array to hold the disk arrangement
     towers = []
 
     # Overwrite std class functions
     def __init__(self, n) -> None:
         self.n = n
-        self.towers = np.zeros((n, 3), dtype=np.int16)
+        self.zero = n + 1
+        self.towers = np.ones((n, 3), dtype=np.int16)
+        self.towers *= self.zero
         self.towers[:,0] = np.arange(1, n + 1)
     def __str__(self):
         rstr = "-----------------\n"
         for i in range(self.n):
             for t in range(3):
                 d = (int) (self.towers[i,t])
-                if d != 0:
+                if d != self.zero:
                     rstr += format(d)
                 else:
                     rstr += "|"
@@ -37,41 +44,56 @@ class TowersOfHanoi:
         ind = 0
         while ind < self.n:
             rint = self.towers[ind, tower]
-            if rint != 0:
+            if rint != self.zero:
                 return rint
             else:
                 ind += 1
-        return 0
+        return self.zero
     
     # Return the top disk of each tower
     def top_row(self):
         rarr = np.zeros(3)
-        for i in range(rarr.size):
-            rarr[i] = self.top(i)
+        for t in range(3):
+            rarr[t] = self.top(t)
         return rarr
     
-    # Return the tower of the max value in the top row
-    def ind_top_max(self):
-        tmax = 0
-        tind = 0
-        for i in range(3):
-            ttop = self.top(i)
-            if ttop > tmax:
-                tmax = ttop
-                tind = i
-        return tind
+    # Return the tower of the smalled value(1) in top row
+    def top_min(self):
+        top_row = self.top_row()
+        tmin = self.zero
+        tm = 0 
+        for t in range(top_row.size):
+            if top_row[t] < tmin:
+                tmin = top_row[t]
+                tm = t
+        return tm
+
+    # Return the tower of the mid value in the top row
+    def top_mid(self):
+        top_row = self.top_row()
+        tmin = self.top_min()
+        tmid = self.zero
+        tm = 0
+        for t in range(top_row.size):
+            if t != tmin:
+                if tmid > top_row[t]:
+                    tmid = top_row[t]
+                    tm = t
+        #print(f"top_mid(): top_row:{top_row}, tmin:{tmin}, tMID:{tm}")
+        return tm
 
     # Return the number of disks in tower
     def get_stack_size(self, tower):
         ind = 0
         while ind < self.n:
-            if self.towers[ind, tower] != 0:
+            if self.towers[ind, tower] != self.zero:
                 return self.n - ind
             ind += 1
         return 0
 
-    # Simulate moving of a value from from_Tower to to_Tower
+    # Move the top disk from fr_Tower >>>to>>> to_Tower
     def move(self, tFr, tTo):
+        print(f"MOVE| tFR:{tFr}, tTo:{tTo}")
         # Get Top diskspace indices from specified towers
         iFr, iTo = -1, -1
         # Set starting indices
@@ -80,12 +102,12 @@ class TowersOfHanoi:
         indTop = 0
         indBot = self.n - 1
         while iFr < 0 or iTo < 0:
-            if self.towers[indTop, tFr] != 0 and iFr < 0:
+            if self.towers[indTop, tFr] != self.zero and iFr < 0:
                 iFr = indTop
             else:
                 #print(f"Tower[{indTop},{tFr}]:{self.towers[indTop, tFr]}, iFr:{iFr}")
                 indTop += 1
-            if self.towers[indBot, tTo] == 0 and iTo < 0:
+            if self.towers[indBot, tTo] == self.zero and iTo < 0:
                 iTo = indBot
             else:
                 #print(f"Tower[{indBot},{tTo}]:{self.towers[indBot, tTo]}, iTo:{iTo}")
@@ -95,16 +117,44 @@ class TowersOfHanoi:
         self.towers[iFr, tFr], self.towers[iTo, tTo] = self.towers[iTo, tTo], self.towers[iFr, tFr]
         print(self.__str__()) # TEST: Print layout post-Move
 
+    # Define a function to shift the smallest disk (1) right by NP places
+    #   - NP = 1 if n is even
+    #   - NP = 2 if n is odd
+    def shift_smallest_right(self):
+        NP = 0
+        if self.n % 2 == 0:
+            NP = 1
+        else:
+            NP = 2
+        
+        t1 = self.top_min()
+        pos_to = NP + t1
+        if pos_to > 2:
+            pos_to -= 3
+        self.move(t1, pos_to)
+    
+    # Define a function to shift the middle disk (1 < mid < max) right to the
+    # first available space
+    def shift_middle_right(self):
+        tm = self.top_mid()
+        t1 = self.top_min()
+        pos_to = 3 - tm - t1
+        #print(f"tm:{tm}, t1:{t1}, pos_to:{pos_to}")
+        self.move(tm, pos_to)
+
     # Solve the puzzle iteratively
     def solve_iterative(self):
-        priority = np.array([0,1,2])
-        for i in range(self.minimum_moves()):
-            pass
-
+        loops = (int) (self.minimum_moves()/2)
+        for i in range(loops):
+            self.shift_smallest_right()
+            self.shift_middle_right()
+        self.shift_smallest_right()
+        #print(self)
+    
     # Solve the puzzle recursively
     def solve_recursive(self):
         c = 0
-        while self.top(0) == 0 and self.top(1) == 0:
+        while self.top(0) == self.zero and self.top(1) == self.zero:
             if self.top(0) > self.top(2):
                 self.move(0, 2)
                 c += 1
@@ -115,13 +165,12 @@ class TowersOfHanoi:
                 self.move(2, 1)
                 c += 1
         print(f"Solved in {c} moves!")
-    
 
 def main():
     # Test function
     t3 = TowersOfHanoi(3)
     print(t3)
-
+    t3.solve_iterative()
 
     #t5 = TowersOfHanoi(5)
     #print(t5)
