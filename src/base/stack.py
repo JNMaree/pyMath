49f1e1b4b5e8065c44fdbb0a8fcd32750b660289
Johann Maree <jnmaree@protonmail.com>
2021-11-09 02:23:02 +0200

    Stack class method updates

--- src/base/stack.py
+++ src/base/stack.py
@@ -6,36 +6,80 @@ class Stack:
     """
 
     # Define array to hold values
-    __elements = []
+    __elements = np.empty(0)
+
+    # Define an internal counter for tracking elements
+    __n = 0
 
     def __init__(self, array):
         if isinstance(array,(int)):
             self.__elements = np.array((array))
+            self.__n = array
         elif isinstance(array, (list)):
             self.__elements = np.array(array)
+            self.__n = len(array)
         elif isinstance(array, np.ndarray):
             self.__elements = array
+            self.__n = array.size
+    # Native string representation
     def __str__(self) -> str:
-        rstr = ""
-        for i in range(self.__elements.size, -1, -1):
+        rstr = format(self.__n) + ":[ "
+        for i in range(self.__n - 1, -1, -1):
             rstr += format(self.__elements[i])
+            rstr += ", "
+        rstr.removesuffix(", ")
+        rstr += "]"
         return rstr
         
     # Push:
     #   - add element to stack
     def push(self, value):
-        np.append(self.__elements, value)
+        self.__elements = np.append(self.__elements, value)
+        self.__n += 1
 
     # Pop:
     #   - remove and return last element from stack
     def pop(self) -> __elements.dtype:
-        rt = self.__elements[self.__elements.size - 1]
-        self.__elements = np.delete(self.__elements, self.__elements.size - 1)
-        return rt
+        if self.__n > 0:
+            rt = self.__elements[self.__n - 1]
+            self.__elements = np.delete(self.__elements, self.__n - 1)
+            self.__n -= 1
+            return rt
 
     # Check whether or not stack is empty
     def empty(self) -> bool:
-        if self.__elements.size == 0:
+        if self.__n == 0:
             return True
         else:
             return False
+    
+def main():
+    # Stack test functions
+    t = np.arange(2,10)
+    print(f"t:{t}")
+    ts = Stack(t)
+    print(f"stack_t:{ts}")
+
+    # Test Push functions
+    i1 = 12
+    i2 = 16
+    i3 = 20
+    print(f"push: i1:{i1}, i2:{i2}, i3:{i3}")
+    ts.push(12)
+    ts.push(16)
+    ts.push(20)
+    print(f"stack_ts:{ts}")
+
+    # Test Pop functions
+    countPop = 4
+    print(f"pop: {countPop} elements:")
+    for i in range(countPop):
+        print(f"pop:{ts.pop()},", end=" ")
+    print(f"\nstack_ts:{ts}")
+
+    print("pop: till empty")
+    while not ts.empty():
+        print(f"{ts.pop()}, ts:{ts}")
+
+if __name__ == "__main__":
+    main()
