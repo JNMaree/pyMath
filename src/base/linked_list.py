9b0c37ddcff68c5e7c6c838c3c329640df369f5a
Johann Maree <jnmaree@protonmail.com>
2021-11-12 23:55:03 +0200

    linked list method udpate

--- src/base/linked_list.py
+++ src/base/linked_list.py
@@ -3,10 +3,35 @@ import numpy as np
 class LinkedList:
     """
         Sequence of Data Structures
+            - Emulated using a 2D array
+
     """
 
-    # Define a starting link
+    # Define a starting node
     __head = []
 
-    # Define an array of elements
+    # Define the array of elements
     __elements = []
+
+    def __init__(self) -> None:
+        self.__elements = np.zeros()
+        self.__head = None
+    
+    def __str__(self) -> str:
+        pass
+
+    def insert(self, value):
+        pass
+
+    def delete(self, value):
+        pass
+
+    def search(self, value):
+        pass
+
+def main():
+    # Test functions
+    pass
+
+if __name__ == "__main__":
+    main() 
