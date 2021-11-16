b33372774c22ebcd80beac5705a9dc5a1f3a05f6
Johann Maree <jnmaree@protonmail.com>
2021-11-16 09:52:36 +0200

    FISR algorithm fixed with numpy types

--- src/tasks/fisr.py
+++ src/tasks/fisr.py
@@ -1,28 +1,29 @@
 import math
+import numpy as np
 
-def InverseSqrt(x):
+def FastInverseSqrt(x):
     """
-    Quake Function: Fast Inverse Square Root (FISR) approximation
+    Quake III Function: Fast Inverse Square Root (FISR) approximation
         - Approximates the inverse root of a float using Newton's method:
             
                 x_(n+1) = x_n - f(x_n)/f'(x_n)
 
         - where the function f(x_n) is = 1/(x_n)^2 - y
     """
-    x_d2 = 0.5 * x                  # Half of X
+    x_d2 = 0.5 * x                          # Half of x
     
-    i = x                           # Convert x to integer
-    
-    i = 0x5F3759DF - (i >> 1)       # Set initial guess
+    i = np.float32(x)                       # Convert x to integer
+    i = i.view(np.int32)
 
-    x_n = float (i)                   # Convert back to float
-    
-    x_n = x_n * (1.5 - x_d2*x_n*x_n)        # Calculate Newton's Method once       
+    j = np.int32(0x5f3759df - (i >> 1))     # Set initial guess
+    x_n = j.view(np.float32)                # Convert back to float
+
+    x_n = x_n * (1.5 - (x_d2 * x_n * x_n))  # One run of Newton's Method 
     return x_n
 
 def main():
     
-    x = [1, 3, 11, 101, 4761, 10000]
+    x = [1, 3, 11, 256, 4761, 10000]
     n = len(x)
 
     # Precise Calculation Solutions
@@ -33,12 +34,12 @@ def main():
     # Approximation Solutions
     x_inv = [0]*n
     for i in range(n):      # Calculate Inverse Square root using FISR
-        x_inv[i] = InverseSqrt(x[i])
+        x_inv[i] = FastInverseSqrt(x[i])
 
     # Comparison of Precise and Approximate Solutions
-    x_comp = [0]*n
+    x_compare = [0]*n
     for i in range(n):      # Compare the difference between the two methods
-        x_comp[i] = abs(x_def[i] - x_inv[i])
+        x_compare[i] = abs(x_def[i] - x_inv[i])
 
     # Printout results
     print("x\tisqrt(x)_calculated\tisqrt(x)_approximated\tisqrt_difference")
@@ -46,7 +47,7 @@ def main():
         print(f"{ x[i] }", end="\t")
         print(f" { x_def[i] :.9f}", end="\t\t")
         print(f" { x_inv[i] :.6e}", end="\t\t")
-        print(f" { x_comp[i] :.6e}")
+        print(f" { x_compare[i] :.6e}")
 
 if __name__ == "__main__":
     main()
