6a1e2e0ca6dcc59610e4beb1b9ab375534a5deb7
Johann Maree <jnmaree@protonmail.com>
2021-11-17 11:09:18 +0200

    FISR comment and test updates

--- src/tasks/fisr.py
+++ src/tasks/fisr.py
@@ -12,17 +12,17 @@ def FastInverseSqrt(x):
     """
     x_d2 = 0.5 * x                          # Half of x
     
-    i = np.float32(x)                       # Convert x to integer
-    i = i.view(np.int32)
+    i = np.float32(x)                       # Convert x to numpy (c type) float
+    i = i.view(np.int32)                    # View x as an integer
 
-    j = np.int32(0x5f3759df - (i >> 1))     # Set initial guess
-    x_n = j.view(np.float32)                # Convert back to float
+    j = np.int32(0x5f3759df - (i >> 1))     # Set initial guess for approximation
+    x_n = j.view(np.float32)                # View x as float again
 
-    x_n = x_n * (1.5 - (x_d2 * x_n * x_n))  # One run of Newton's Method 
+    x_n = x_n * (1.5 - (x_d2 * x_n * x_n))  # One run of Newton's Method to enhance accuracy
     return x_n
 
 def main():
-    
+    # Array of Test roots:
     x = [1, 3, 11, 256, 4761, 10000]
     n = len(x)
 
@@ -37,17 +37,22 @@ def main():
         x_inv[i] = FastInverseSqrt(x[i])
 
     # Comparison of Precise and Approximate Solutions
-    x_compare = [0]*n
+    x_cmp = [0]*n
     for i in range(n):      # Compare the difference between the two methods
-        x_compare[i] = abs(x_def[i] - x_inv[i])
+        x_cmp[i] = abs(x_def[i] - x_inv[i])
+
+    x_percentage_diff = [0]*n
+    for i in range(n):      # Calculate accuracy as a percentage of the definitive solution
+        x_percentage_diff[i] = abs(x_def[i] - x_cmp[i])/(x_def[i]) * 100
 
     # Printout results
-    print("x\tisqrt(x)_calculated\tisqrt(x)_approximated\tisqrt_difference")
+    print("x\tisqrt(x)_calculated\tisqrt(x)_approximated\tisqrt_difference\tPercentage(%)")
     for i in range(n):
         print(f"{ x[i] }", end="\t")
         print(f" { x_def[i] :.9f}", end="\t\t")
         print(f" { x_inv[i] :.6e}", end="\t\t")
-        print(f" { x_compare[i] :.6e}")
+        print(f" { x_cmp[i] :.6e}", end="\t\t")
+        print(f" { x_percentage_diff[i] :.3f}")
 
 if __name__ == "__main__":
     main()
