aee2b18fdd92a18a9b0045f8620c2acca7e3172a
Johann Maree <jnmaree@protonmail.com>
2021-12-01 21:44:24 +0200

    aoc day1 comment updates

--- src/tasks/advent_of_code_21/day1.py
+++ src/tasks/advent_of_code_21/day1.py
@@ -1,11 +1,12 @@
-import os
 import numpy as np
 
+# Read input file of ints & store in numpy array
 def read_int_input(fname):
     with open(fname, 'r') as f:
         arr = np.array(np.uint16 (f.read().splitlines()))
     return arr
 
+# Count increases
 def count_inc(arr):
     inc = 0
     for i in range(1, arr.size):
@@ -13,6 +14,7 @@ def count_inc(arr):
             inc += 1
     return inc
 
+# Count sum( of ints in specified window) increases
 def count_window_inc(wsize, arr):
     inc = 0
     wst = 0
@@ -23,7 +25,7 @@ def count_window_inc(wsize, arr):
         if wsum > wst:
             inc += 1
         wst = wsum
-    return inc 
+    return inc
         
 
 def main():
@@ -32,11 +34,17 @@ def main():
     depths = read_int_input(relative_path + 'day1_input.txt')
     print(depths)
 
+    # Define a window size
+    window = 3
+
+    # Use test array to test methods
     test_arr = np.array([199,200,208,210,200,207,240,269,260,263])
     #print(test_arr)
-    #print(f"count_increases:{count_inc(test_arr)}")
+    #print(f"testc_increases:{count_inc(test_arr)}")
+    #print(f"testc_window[{window}]_inc:{count_window_inc(window, test_arr)}")
+
+    # Print data outputs 
     print(f"count_increases:{count_inc(depths)}")
-    window = 3
     print(f"count_window[{window}]_inc:{count_window_inc(window, depths)}")
 
 
