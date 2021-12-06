042c184f109b7e7851316bf2cf66d3fdaa6673c8
Johann Maree <jnmaree@protonmail.com>
2021-12-06 13:23:54 +0200

    aoc day6 complete

--- src/tasks/advent_of_code_21/day6.py
+++ src/tasks/advent_of_code_21/day6.py
@@ -1,45 +1,34 @@
-#import numpy as np
 import time
 
 class FishArray:
 
-    fish = []
+    fish_states = []
+
+    max_state = 9
+
+    start_state = 6
+    new_state = 8
 
     n = 0
 
-    capc = 0
-
     def __init__(self, array) -> None:
-        self.n = len(array) 
-        self.capc = 2*self.n
-        self.fish = [0]*self.capc
-        self.fish[:self.n] = array
+        self.fish_states = [0] * self.max_state
+        for i in array:
+            self.fish_states[i] += 1
+            self.n += 1
 
     def __str__(self) -> str:
         sret = format(self.n)
-        sret += format(self.fish)
+        sret += format(self.fish_states)
         return sret
 
     def elapse_day(self):
-        new_fish = 0
-        for i in range(self.n):
-            if self.fish[i] > 0:
-                self.fish[i] -= 1
-            else:
-                self.fish[i] = 6
-                new_fish += 1 
-        additions = [8] * new_fish 
-        new_n = self.n + new_fish
-        if new_n > self.capc:
-            self.capc *= 2
-            new_array = [0]*self.capc 
-            new_array[:self.n] = self.fish[:self.n]
-            new_array[self.n:new_n] = additions
-            self.fish = new_array
-            self.n = new_n
-        else:
-            self.fish[self.n:new_n] = additions
-            self.n = new_n
+        new_fish = self.fish_states[0]
+        for i in range(self.max_state - 1):
+            self.fish_states[i] = self.fish_states[i + 1]
+        self.fish_states[self.start_state] += new_fish
+        self.fish_states[self.new_state] = new_fish
+        self.n += new_fish
 
 def main():
 
@@ -57,17 +46,16 @@ def main():
     days = 80
     for d in range(days):
         lanternFish.elapse_day()
-        print(f"day {d}| {lanternFish.n}")
+        print(f"day {d}| {lanternFish}")
     print(f"After {days} days, there are {lanternFish.n} fish.")
 
     # Part 2 --------------------------------------------------------
-    """
+    # Increase number of days to 256
     days2 = 256
     for d in range(days + 1, days2 + 1):
         lanternFish.elapse_day()
         print(f"day {d}| {lanternFish}")
-    print(f"After {days} days, there are {lanternFish.n} fish.")   
-    """
+    print(f"After {days2} days, there are {lanternFish.n} fish.")
 
     # Calculate performance by time elapsed
     t1 = time.perf_counter()
