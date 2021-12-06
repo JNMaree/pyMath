27ba3bf4a80efc2f899d68e106a5972dc209e5f4
Johann Maree <jnmaree@protonmail.com>
2021-12-06 11:41:09 +0200

    aoc day6 part1 complete

---
+++ src/tasks/advent_of_code_21/day6.py
@@ -0,0 +1,70 @@
+from types import new_class
+import numpy as np
+
+class FishArray:
+
+    fish = []
+
+    n = 0
+
+    capc = 0
+
+    def __init__(self, array) -> None:
+        self.n = array.size
+        self.capc = 2*self.n
+        self.fish = np.zeros(self.capc, dtype=np.uint8)
+        self.fish[:self.n] = array
+
+    def __str__(self) -> str:
+        sret = format(self.n)
+        sret += format(self.fish)
+        return sret
+
+    def elapse_day(self):
+        new_fish = 0
+        for i in range(self.n):
+            if self.fish[i] > 0:
+                self.fish[i] -= 1
+            else:
+                self.fish[i] = 6
+                new_fish += 1 
+        additions = np.full(new_fish, 8)
+        new_n = self.n + new_fish
+        if new_n > self.capc:
+            self.capc *= 2
+            new_array = np.zeros(self.capc)
+            new_array[:self.n] = self.fish[:self.n]
+            new_array[self.n:new_n] = additions
+            self.fish = new_array
+            self.n = new_n
+        else:
+            self.fish[self.n:new_n] = additions
+            self.n = new_n
+
+def main():
+
+    fish = []
+    
+    relative_path = 'src/tasks/advent_of_code_21/day6_input.txt'
+    with open(relative_path, 'r') as f:
+        fish = np.array(np.uint8 (f.read().split(',')))
+    
+    lanternFish = FishArray(fish)    
+
+    days = 80
+    for d in range(days):
+        lanternFish.elapse_day()
+        print(f"day {d}| {lanternFish}")
+    print(f"After {days} days, there are {lanternFish.n} fish.")
+
+    # Part 2 --------------------------------------------------------
+    """
+    days2 = 256
+    for d in range(days + 1, days2 + 1):
+        lanternFish.elapse_day()
+        print(f"day {d}| {lanternFish}")
+    print(f"After {days} days, there are {lanternFish.n} fish.")   
+    """
+
+if __name__ == "__main__":
+    main()
